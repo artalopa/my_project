8af2645986febcc1e17e14b67af429b68a119883
artalopa <ryanartalopa@gmail.com>
2024-10-20 22:35:21 +0700

    s3 filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,12 +47,12 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
+            'key' => env('AKIA3FRRIWIPKSMHVOND'),
+            'secret' => env('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'),
+            'region' => env('ap-southeast-1'),
+            'bucket' => env('myproject-artalopa'),
+            'url' => env('https://myproject-artalopa.s3.amazonaws.com'),
+            'endpoint' => env('http://myproject-artalopa.s3-website-ap-southeast-1.amazonaws.com'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
